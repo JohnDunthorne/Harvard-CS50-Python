7f932f5a03fe55248e7049bd7bc1fac423b0006c
John Dunthorne <johndunthorne1@gmail.com>
2023-09-15 17:57:00 -0500

    1

--- Hour1.py
+++ Hour1.py
@@ -6,6 +6,8 @@ name = name.strip()
 
 # Make sure name is capitalised properly
 name = name.capitalize()
+# Perhaps it'll be a 2 name input
+name = name.title()
 
 # Say hello to user
 print("hello " + name)
