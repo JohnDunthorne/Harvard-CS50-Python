21b81002a4df6409ab7f53aa6d0e70920bffae16
John Dunthorne <johndunthorne1@gmail.com>
2023-09-16 12:03:55 -0500

    1

--- Hour2.py
+++ Hour2.py
@@ -1,5 +1,8 @@
 # Ask user for their name
 name = input("Name: ").strip().title()
 
+# Split user name into first and last name
+first, last = name.split()
+
 # Say hello to user "Sarcastically" ;)
 print(f"Hello \"{name}\"")
