86278a7f019fef1c79efc8ec4c8add97308550ac
John Dunthorne <johndunthorne1@gmail.com>
2023-09-17 12:39:00 -0500

    1

---
+++ Hour2.16.py
@@ -0,0 +1,14 @@
+# Show a strudent their grade based on thier score
+
+score = 45
+
+if score >= 90:
+    print("you got an A")
+elif score >= 80:
+    print("you got a B")
+elif score >= 70:
+    print("you got a C")  
+elif score >= 60:
+    print("you got a D")
+else:
+    print("you got an F")                    
