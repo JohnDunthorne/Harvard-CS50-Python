6e8347b2b1df5793ff28dc43eef801920398f3fd
John Dunthorne <johndunthorne1@gmail.com>
2023-09-17 12:19:42 -0500

    1

--- Hour1.50.py
+++ Hour1.50.py
@@ -1,10 +1,18 @@
 # Compare some things
 
 x = 3
-y = 3
+y = 4
+
 if x > y:
     print("x bigger than y")
 elif y > x:
     print("x smaller than y")
 else:
     print("x is equal to y")
+
+# Check to see if x is equal to y, or not
+
+if x > y or x < y:
+    print("X is not equal to Y")
+else:
+    print("X is equal to Y")
