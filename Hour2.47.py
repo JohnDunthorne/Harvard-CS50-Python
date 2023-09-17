20521e7bddd799d364b9200682441b41a2cdf9dd
John Dunthorne <johndunthorne1@gmail.com>
2023-09-17 18:09:30 -0500

    1

--- Hour2.47.py
+++ Hour2.47.py
@@ -4,4 +4,11 @@ meow_left = 7
 while meow_left > 0:
     print("Meow")
     meow_left -= 1
+
+# Below produces the same result
+# Counting up from 0 is more conventional
+meows_to_go = 0
+while meows_to_go < 7:
+    print("MEOW")
+    meows_to_go += 1    
     
