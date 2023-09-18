9696b72eca6addd70f047391a9f2160a92d2c214
John Dunthorne <johndunthorne1@gmail.com>
2023-09-17 19:09:55 -0500

    1

--- Hour3.22.py
+++ Hour3.22.py
@@ -4,3 +4,8 @@ students = ["Hermione", "Harry", "Ron"]
 
 for student in students:
     print(student)
+
+# Rank these students in the order they are in the list
+
+for i in range(len(students)):
+    print(i + 1, students[i])   
