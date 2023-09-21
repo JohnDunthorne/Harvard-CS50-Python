9ed94bfb3914d6f878ab678db5318cf8de7c5bcd
John Dunthorne <johndunthorne1@gmail.com>
2023-09-21 18:30:35 -0500

    1

--- 4.37.py
+++ 4.37.py
@@ -1,14 +1,14 @@
 # Define a function to get an integer from user
 
 def main():
-    user_number = get_int()
+    user_number = get_int("Pick a Number: ")
     print(f"Your number is {user_number}")
 
 
-def get_int():
+def get_int(user_prompt):
     while True:
         try:
-            user_number = int(input("Pick a number: "))
+            user_number = int(input(user_prompt))
         except ValueError:
             pass
         else: 
