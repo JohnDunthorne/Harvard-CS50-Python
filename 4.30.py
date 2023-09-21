a9a5ebcddc5e27b7a5f4f047aa3d9205d926d5bb
John Dunthorne <johndunthorne1@gmail.com>
2023-09-21 18:07:19 -0500

    q

--- 4.30.py
+++ 4.30.py
@@ -1,2 +1,12 @@
 # Prompt the user to provide a number, until they do.
 
+while True:
+    try:
+        x =  int(input("Pick a number: "))
+    except ValueError:
+        print("Please only type a number")
+    else: 
+        break   
+        
+        
+print(f"x is {x}")
