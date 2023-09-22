fda0a52e6f5c48ea6880f39c7dbe0eef78363216
John Dunthorne <johndunthorne1@gmail.com>
2023-09-22 11:38:24 -0500

    1

---
+++ 5.02.py
@@ -0,0 +1,15 @@
+# use random.randint to select a random number
+
+from random import randint
+from random import shuffle
+
+
+random_number = randint(1,100)
+print(random_number)
+
+# use random shuffle to a list
+
+cards = ["one of hearts", "two of hearts", "three of hearts", f"our of hearts", "five of hearts", "six of hearts"]
+shuffle(cards)
+for card in cards:    
+    print(card)
