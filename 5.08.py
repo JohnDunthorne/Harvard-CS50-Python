ea831abfa4d79e067e255bcdfd2587947489524c
John Dunthorne <johndunthorne1@gmail.com>
2023-09-22 11:49:16 -0500

    1

---
+++ 5.08.py
@@ -0,0 +1,12 @@
+# Find out the average of a set of numbers
+
+from statistics import mean
+
+numbers = mean([50, 100, 200])
+print(int(numbers))
+
+
+# you can do it on one line if you dont
+# need to reuse the variable
+
+print(int(mean([10, 20, 30])))
