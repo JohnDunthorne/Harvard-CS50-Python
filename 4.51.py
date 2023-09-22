52abd7d6238293bd02323bd8d08e12035e1a08f7
John Dunthorne <johndunthorne1@gmail.com>
2023-09-22 07:29:19 -0500

    1

---
+++ 4.51.py
@@ -0,0 +1,8 @@
+# Simpulate a dice roll with random.choice
+
+import random
+def main():
+    dice_roll()
+    print(dice_roll)
+def dice_roll():    
+    dice_roll = random.choice([1, 2, 3, 4, 5, 6])
