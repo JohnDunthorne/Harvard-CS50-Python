7c4eb9fd772336ad287d6db19b1fcf77aa2cfce1
John Dunthorne <johndunthorne1@gmail.com>
2023-09-22 11:39:53 -0500

    1

--- 5.02.py
+++ 5.02.py
@@ -9,7 +9,7 @@ print(random_number)
 
 # use random shuffle to a list
 
-cards = ["one of hearts", "two of hearts", "three of hearts", f"our of hearts", "five of hearts", "six of hearts"]
+cards = ["one of hearts", "two of hearts", "three of hearts", "four of hearts", "five of hearts", "six of hearts"]
 shuffle(cards)
 for card in cards:    
     print(card)
