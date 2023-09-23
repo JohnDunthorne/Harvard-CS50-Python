4bb964530d812dda92438c04227ddd7e3ee18bd0
John Dunthorne <johndunthorne1@gmail.com>
2023-09-23 10:10:04 -0500

    1

--- 5.45.py
+++ 5.45.py
@@ -1,12 +1,15 @@
 # API's are application programming interface
 # *** pip install requests ***
-
+import json
 import requests
 import sys
 
 # if user does not provide the name of the file and the name of
 # a band, then exit the program
-if len(sys.argv) != 2:
-    sys.exit()
+if sys.argv != 2:
+    sys.exit
 
-requests.get("https://itunes.apple.com/search?entity=songs&limit=1&term=", sys.argv[1])
+response = requests.get("https://itunes.apple.com/search?term=" + sys.argv[1] + "&limit=1.")
+print(json.dumps(response.json(), indent=2))
+
+# This url doesnt work now for some reason
