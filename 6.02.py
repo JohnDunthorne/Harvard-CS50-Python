0ce81de662d50fbf300da8f50f9e28e4a3cffce8
John Dunthorne <johndunthorne1@gmail.com>
2023-09-23 10:37:24 -0500

    1

---
+++ 6.02.py
@@ -0,0 +1,13 @@
+# Create some functions you can use elsewhere
+
+def main():
+    hello("John")
+    goodbye("John")
+
+def hello(name):
+    print(f"Hello, {name}")
+
+def goodbye(name):
+    print(f"Goodbye, {name}")
+
+main()
