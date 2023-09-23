bd1cdc75d9fc0125a55069f37b920d8de2bb5b23
John Dunthorne <johndunthorne1@gmail.com>
2023-09-22 19:46:07 -0500

    1

--- 5.11.py
+++ 5.11.py
@@ -2,15 +2,21 @@
 
 import sys
 
+# Check for potential errors
 if len(sys.argv) < 2:
-    print("Please add your name")
+    sys.exit("Please add your name")
 elif len(sys.argv) > 2:
-    print("please provide first name only")
-else:
-    print("Hello, my name is", sys.argv[1])
+    sys.exit("please provide first name only")
+
+# Run code line
+print("Hello, my name is", sys.argv[1])
 
 # Type ***python 5.11.py John***
 # for this to work
 
 # note we are pulling index [1] since the name of the program
 # itself is at index [0]
+
+# sys.exit will  work like print but then end the program
+# this way once python receives the correcty information
+# it will run the print function on line 12
