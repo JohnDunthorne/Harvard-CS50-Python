11d116b010ca452b60051c99481bbea79e0d329c
John Dunthorne <johndunthorne1@gmail.com>
2023-09-23 09:46:48 -0500

    1

--- 5.45.py
+++ 5.45.py
@@ -1,1 +1,12 @@
 # API's are application programming interface
+# *** pip install requests ***
+
+import requests
+import sys
+
+# if user does not provide the name of the file and the name of
+# a band, then exit the program
+if len(sys.argv) != 2:
+    sys.exit()
+
+requests.get(https://itunes.apple.comsearch?entity=songs&limit=1&term=sys.argv[1])
