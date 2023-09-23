3143d804485a73911e2255f6627d8cb487a0a5ec
John Dunthorne <johndunthorne1@gmail.com>
2023-09-22 19:36:52 -0500

    1

--- 5.11.py
+++ 5.11.py
@@ -2,7 +2,15 @@
 
 import sys
 
-print("Hello, my name is", sys.argv[1])
+if len(sys.argv) < 2:
+    print("Please add your name")
+elif len(sys.argv) > 2:
+    print("please provide first name only")
+else:
+    print("Hello, my name is", sys.argv[1])
 
 # Type ***python 5.11.py John***
 # for this to work
+
+# note we are pulling index [1] since the name of the program
+# itself is at index [0]
