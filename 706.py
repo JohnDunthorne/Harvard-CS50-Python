cfe36b12eed729de38f55ba2cf945ac575f7ef17
John Dunthorne <johndunthorne1@gmail.com>
2023-09-24 11:15:43 -0500

    1

--- 706.py
+++ 706.py
@@ -1,2 +1,18 @@
 # use file I/O to read and write stored memory
 
+name = input("Type your name: ")
+
+# open a file that youd like to save
+# the name to,
+# we write "w" as a second argument
+# to let python know we are writing
+# to the file
+file = open("names.txt", "w")
+# we assign open a variable so we can
+# handle it.
+
+# Write the selected variable to the file
+file.write(name)
+
+# Then close the file so we can save our name
+file.close()
