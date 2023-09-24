80532f0b608599c2c5f7838f1d0d89ea59ea815f
John Dunthorne <johndunthorne1@gmail.com>
2023-09-24 10:16:00 -0500

    1

--- 649.py
+++ 649.py
@@ -2,6 +2,7 @@
 
 from functions import hello_name
 
-def test_hello_name():
+def test_hello_name_input():
     assert hello_name("John") == "Hello, John"
+def test_hello_name_default():
     assert hello_name() == "Hello, World"
