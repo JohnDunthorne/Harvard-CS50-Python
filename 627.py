e16db0828872a3fdab2f1ec8a796618aee91c778
John Dunthorne <johndunthorne1@gmail.com>
2023-09-23 20:20:49 -0500

    1

--- 627.py
+++ 627.py
@@ -4,11 +4,13 @@
 # docs.pytest.org for documentation
 from functions import square
 
-def test_square():  
+def test_positive():  
     assert square(2) == 4
-    assert square(-2) == 4
     assert square(3) == 9
+def test_negative():
+    assert square(-2) == 4
     assert square(-3) == 9
+def test_zero():
     assert square(0) == 0
 
 # to test this with pytest
