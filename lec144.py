0cf95269e1954a81b8007941d34afe3100905851
John Dunthorne <johndunthorne1@gmail.com>
2023-10-15 08:29:14 -0500

    1

--- lec144.py
+++ lec144.py
@@ -1,6 +1,24 @@
 from sys import argv
+from sys import exit
 
-if len(argv) == 2:
-    print(f"hello", argv[1])
+def multiply(a,b):
+    result = a * b
+    return result
+
+if len(argv) != 3:
+    print("Need two integers")
+    exit(1)
+
+firststr = argv[1]
+secondstr = argv[2]
+
+try:
+    firstint = int(firststr)
+    secondint = int(secondstr)
+except ValueError:
+    print("Expected numbers only")
+
+if len(argv) == 3:
+    print(multiply(firstint, secondint))
 else:
-    print("hello world")
+    print("type in 2 numbers")
