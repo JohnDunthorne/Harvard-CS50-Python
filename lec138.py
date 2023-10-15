c956c054132d687a1648e636ac87bfb8cf9e449f
John Dunthorne <johndunthorne1@gmail.com>
2023-10-14 19:21:25 -0500

    1

--- lec138.py
+++ lec138.py
@@ -1,27 +1,24 @@
 scores = []
-error = print("numbers only")
-try:
-    for i in range(3):
-        score = int(input("Score: "))
+
+for i in range(3):
+    score = int(input("Score: "))
+    if 0 <= score <= 100:
         scores.append(score)
-except ValueError:
-    error
-
-try: 
-    average = sum(scores)/len(scores)
-except ZeroDivisionError:
-    error
-
-try:
-    if average >= 90:
-        print("A")
-    elif average >= 80:
-        print("B")    
-    elif average >= 70:
-        print("C")
-    elif average >= 60:
-        print("D")
     else:
-        print("F")
-except NameError:
-    error
+        print("Invalid input")
+        scores = [-999, -999, -999]
+    
+average = sum(scores)/len(scores)
+
+if average >= 90:
+    print("A")
+elif average >= 80:
+    print("B")    
+elif average >= 70:
+    print("C")
+elif average >= 60:
+    print("D")
+elif 0 <= average < 60:
+    print("F")    
+else:
+    print("You did something wrong")
