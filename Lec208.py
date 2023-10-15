6023f031b3da3fd26eaf0dd1ab60604dcadc06bc
John Dunthorne <johndunthorne1@gmail.com>
2023-10-15 09:28:25 -0500

    1

--- Lec208.py
+++ Lec208.py
@@ -2,7 +2,7 @@ import csv
 
 with open("phonebook.csv", "a") as file:
     name = input("Name: ")
-    number = input("number")
+    number = input("number: ")
     
     writer = csv.writer(file)
-    
+    writer.writerow([name, number])
