60ae766b45acadc5cae8975051c4d2e50df65e3e
John Dunthorne <johndunthorne1@gmail.com>
2023-10-15 08:38:43 -0500

    1

--- lec144.py
+++ lec144.py
@@ -1,24 +1,24 @@
-from sys import argv
-from sys import exit
+import sys
 
-def multiply(a,b):
-    result = a * b
-    return result
+def multiply(a, b):
+    return a * b
 
-if len(argv) != 3:
-    print("Need two integers")
-    exit(1)
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <number1> <number2>")
+        sys.exit(1)
 
-firststr = argv[1]
-secondstr = argv[2]
+    first_str, second_str = sys.argv[1], sys.argv[2]
 
-try:
-    firstint = int(firststr)
-    secondint = int(secondstr)
-except ValueError:
-    print("Expected numbers only")
+    try:
+        first_int = int(first_str)
+        second_int = int(second_str)
+    except ValueError:
+        print("Error: Both arguments should be valid integers.")
+        sys.exit(1)
 
-if len(argv) == 3:
-    print(multiply(firstint, secondint))
-else:
-    print("type in 2 numbers")
+    result = multiply(first_int, second_int)
+    print(f"The result of {first_int} * {second_int} is: {result}")
+
+if __name__ == "__main__":
+    main()
